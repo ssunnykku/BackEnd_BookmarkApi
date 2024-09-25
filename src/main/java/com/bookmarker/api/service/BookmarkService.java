6a5e2a8b994a4523c779bb3e57839188b942f8c6
ssunnykku <ssunnykku@gmail.com>
2024-09-25 16:43:37 +0900

    Bookmark Insert

--- src/main/java/com/bookmarker/api/service/BookmarkService.java
+++ src/main/java/com/bookmarker/api/service/BookmarkService.java
@@ -2,10 +2,7 @@ package com.bookmarker.api.service;
 
 import com.bookmarker.api.domain.Bookmark;
 import com.bookmarker.api.domain.BookmarkRepository;
-import com.bookmarker.api.dto.BookmarkDTO;
-import com.bookmarker.api.dto.BookmarkMapper;
-import com.bookmarker.api.dto.BookmarkVM;
-import com.bookmarker.api.dto.BookmarksDTO;
+import com.bookmarker.api.dto.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -14,6 +11,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Instant;
 import java.util.List;
 
 @Service
@@ -45,6 +43,12 @@ public class BookmarkService {
 //        Page<BookmarkDTO> bookmarkPage = repository.findByTitleContainsIgnoreCase(query, pageable);
         Page<BookmarkVM> bookmarkPage = repository.findByTitleContainsIgnoreCase(query, pageable);
         return new BookmarksDTO<>(bookmarkPage);
+    }
+
+    public BookmarkDTO createBookmark(CreateBookmarkRequest request) {
+        Bookmark bookmark = new Bookmark(request.getTitle(), request.getUrl(), Instant.now());
+        Bookmark savedBookmark = repository.save(bookmark);
+        return mapper.toDTO(savedBookmark);
     }
 
 }
