d803694fa968b6f827661162b5b53209c916d5bf
ssunnykku <ssunnykku@gmail.com>
2024-09-25 11:25:07 +0900

    bookmark api

---
+++ src/main/java/com/bookmarker/api/domain/BookmarkRepository.java
@@ -0,0 +1,8 @@
+package com.bookmarker.api.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+}
+
+
