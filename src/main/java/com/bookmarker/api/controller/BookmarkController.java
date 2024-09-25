ad7517347959c51d16395e90c296ec8435544695
ssunnykku <ssunnykku@gmail.com>
2024-09-25 15:43:23 +0900

    searchBookmarks add

--- src/main/java/com/bookmarker/api/controller/BookmarkController.java
+++ src/main/java/com/bookmarker/api/controller/BookmarkController.java
@@ -18,7 +18,11 @@ public class BookmarkController {
     private final BookmarkService bookmarkService;
 
     @GetMapping
-    public BookmarksDTO<?> getBookmarks(@RequestParam(name = "page", defaultValue = "1") Integer page) {
-        return bookmarkService.getBookmarks(page);
+    public BookmarksDTO<?> getBookmarks(@RequestParam(name = "page", defaultValue = "1") Integer page,
+                                        @RequestParam(name = "query", defaultValue = "") String query) {
+        if(query == null || query.isBlank()) {
+            return bookmarkService.getBookmarks(page);
+        }
+        return bookmarkService.searchBookmarks(query, page);
     }
 }
--- src/main/java/com/bookmarker/api/domain/BookmarkRepository.java
+++ src/main/java/com/bookmarker/api/domain/BookmarkRepository.java
@@ -11,6 +11,12 @@ public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
     select new com.bookmarker.api.dto.BookmarkDTO(b.id, b.title, b.url, b.createdAt) from Bookmark b
     """)
     Page<BookmarkDTO> findBookmarks(Pageable pageable);
+
+    @Query("""
+    select new com.bookmarker.api.dto.BookmarkDTO(b.id, b.title, b.url, b.createdAt) from Bookmark b
+    where lower(b.title) like lower(concat('%', :query, '%'))
+    """)
+    Page<BookmarkDTO> searchBookmarks(String query, Pageable pageable);
 }
 
 
