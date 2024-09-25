a3a9add3ba70c158df14bf7038c21f6d5913b63f
ssunnykku <ssunnykku@gmail.com>
2024-09-25 11:49:56 +0900

    pagenation first

--- src/main/java/com/bookmarker/api/service/BookmarkService.java
+++ src/main/java/com/bookmarker/api/service/BookmarkService.java
@@ -3,6 +3,9 @@ package com.bookmarker.api.service;
 import com.bookmarker.api.domain.Bookmark;
 import com.bookmarker.api.domain.BookmarkRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,8 +18,10 @@ public class BookmarkService {
     private final BookmarkRepository repository;
 
     @Transactional(readOnly = true)
-    public List<Bookmark> getBookmarks() {
-        return repository.findAll();
+    public List<Bookmark> getBookmarks(Integer page) {
+        int pageNo = page < 1 ? 0 : page - 1;
+        Pageable pageable = PageRequest.of(pageNo, 10, Sort.Direction.DESC, "id");
+        return repository.findAll(pageable).getContent();
     }
 
 }
