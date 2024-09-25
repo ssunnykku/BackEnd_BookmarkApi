c423fbb7787a2adf6a783042e60f1237e1dad225
ssunnykku <ssunnykku@gmail.com>
2024-09-25 14:50:44 +0900

    BookmarkDTO Mapper add

--- src/main/java/com/bookmarker/api/service/BookmarkService.java
+++ src/main/java/com/bookmarker/api/service/BookmarkService.java
@@ -2,6 +2,8 @@ package com.bookmarker.api.service;
 
 import com.bookmarker.api.domain.Bookmark;
 import com.bookmarker.api.domain.BookmarkRepository;
+import com.bookmarker.api.dto.BookmarkDTO;
+import com.bookmarker.api.dto.BookmarkMapper;
 import com.bookmarker.api.dto.BookmarksDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -18,13 +20,16 @@ import java.util.List;
 @RequiredArgsConstructor
 public class BookmarkService {
     private final BookmarkRepository repository;
+    private final BookmarkMapper mapper;
 
     @Transactional(readOnly = true)
-    public BookmarksDTO getBookmarks(Integer page) {
+    public BookmarksDTO<?> getBookmarks(Integer page) {
         int pageNo = page < 1 ? 0 : page - 1;
         Pageable pageable = PageRequest.of(pageNo, 10, Sort.Direction.DESC, "id");
-        Page<Bookmark> bookmarkPage = repository.findAll(pageable);
-        return new BookmarksDTO(bookmarkPage);
+        Page<BookmarkDTO> bookmarkPage = repository.findAll(pageable)
+                //.map(bookmark -> mapper.toDTO(bookmark));
+                .map(mapper::toDTO);
+        return new BookmarksDTO<>(bookmarkPage);
     }
 
 }
